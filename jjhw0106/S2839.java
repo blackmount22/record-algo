fd1447ed82d4a1268b221991dd52ddad40b92e06
jjhw0106 <66353424+jjhw0106@users.noreply.github.com>
2024-12-29 14:56:34 +0900

    Update S2839.java

--- jjhw0106/S2839.java
+++ jjhw0106/S2839.java
@@ -2,19 +2,14 @@ package baekjoon.bruteforce;
 
 public class S2839 {
     int solution(int n) {
-        int a = n/5;
-        int b = n/3;
-        int min = a+b;
-        for (int i = 0; i <= a; i++) {
-            for (int j = 0; j <= b; j++) {
+        int min = -1;
+        for (int i=0; i<=n/5; i++){
+            for (int j = 0; j <= n / 3; j++) {
                 if(5*i + 3*j == n){
-                    if((i + j)<min){
-                        min = i + j;
-                    }
+                    min = i+j;
                 }
             }
         }
-        if(min == a+b) {return -1;}
-        else {return min;}
+        return min;
     }
 }

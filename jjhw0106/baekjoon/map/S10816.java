b23c7fc2f6168614667a43e47b1dba4a4fe4a74b
jjhw0106 <66353424+jjhw0106@users.noreply.github.com>
2025-01-29 14:48:30 +0900

    Add files via upload
    map의 compute() 메소드 활용하여 리팩토링

--- jjhw0106/baekjoon/map/S10816.java
+++ jjhw0106/baekjoon/map/S10816.java
@@ -8,30 +8,45 @@ import java.util.HashMap;
 
 public class S10816 {
     static void solution(ArrayList<Integer> have, ArrayList<Integer> checkList) throws IOException {
+
+        // GPT 수정 코드
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         HashMap<Integer, Integer> mp = new HashMap<Integer, Integer>();
         StringBuilder sb = new StringBuilder();
         for (Integer i : have) {
-            if(mp.get(i) == null){
-                mp.put(i,1);
-            } else {
-                mp.put(i, mp.get(i)+1);
-            }
+            mp.compute(i, (key, value) -> (value == null) ? 1 : value + 1);
         }
-        int index=0;
         for (Integer i : checkList) {
-            if(mp.get(i)==null){
-                sb.append("0");
-            } else{
-                sb.append(String.valueOf(mp.get(i)));
-            }
-            if (index < checkList.size()-1){
-                sb.append(" ");
-                index++;
-            }
+            sb.append(mp.getOrDefault(i, 0)).append(" ");
         }
-        bw.write(sb.toString());
+        bw.write(sb.toString().trim());
         bw.flush();
         bw.close();
+
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        HashMap<Integer, Integer> mp = new HashMap<Integer, Integer>();
+//        StringBuilder sb = new StringBuilder();
+//        for (Integer i : have) {
+//            if(mp.get(i) == null){
+//                mp.put(i,1);
+//            } else {
+//                mp.put(i, mp.get(i)+1);
+//            }
+//        }
+//        int index=0;
+//        for (Integer i : checkList) {
+//            if(mp.get(i)==null){
+//                sb.append("0");
+//            } else{
+//                sb.append(String.valueOf(mp.get(i)));
+//            }
+//            if (index < checkList.size()-1){
+//                sb.append(" ");
+//                index++;
+//            }
+//        }
+//        bw.write(sb.toString());
+//        bw.flush();
+//        bw.close();
     }
 }
